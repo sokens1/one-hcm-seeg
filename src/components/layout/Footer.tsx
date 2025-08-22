64c84c4e0ddb79d6c5b3bbf30bcba93c731f2edd
edicnx1 <edi@cnx4-0.com>
2025-08-22 13:36:09 +0100

    footer et context de recrutement

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -22,7 +22,7 @@ export function Footer() {
           <div className="flex-1 min-w-[300px] space-y-2">
             <div className="flex items-center gap-3">
               <div className="w-16 h-16 sm:w-20 sm:h-20 flex items-center justify-center">
-                <img src="/LOGO HCM WHITE.png" alt="Logo OneHCM" className="w-full h-full object-contain" />
+                <img src="/LOGO HCM WHITE .png" alt="Logo OneHCM" className="w-full h-full object-contain" />
               </div>
               {/* <div>
                 <h3 className="text-lg font-bold text-white">OneHCM</h3>
@@ -30,7 +30,7 @@ export function Footer() {
               </div> */}
             </div>
             <p className="text-sm text-white/90">
-              Plateforme de recrutement dédié à la détection des <br/>
+              Plateforme de recrutement dédiée à la détection des <br/>
               talents pour créer une passerelle entre l’entreprise <br/>
               et ses potentiels internes.
             </p>
